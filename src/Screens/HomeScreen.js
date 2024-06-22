596e52c62009dc6613189e636a9721b130fc83e5
Marcos Eric de Medeiros <srmarcoserick1@gmail.com>
2024-06-22 17:21:23 -0300

    commit

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Footer from '../../components/patterns/Footer'
-import Link from '../../components/Link'
-import { theme } from '../../theme/theme';
-import { Image, Box, Text, Icon, Input, Button } from '../../theme/components';
+import Footer from '../components/patterns/Footer'
+import Link from '../components/Link'
+import { theme } from '../theme/theme';
+import { Image, Box, Text, Icon, Input, Button } from '../theme/components';
 
 const LOGO_ALURA_URL = '/images/alura-logo.svg';
 const SIDE_IMAGE_URL = '/images/alura-cases.png'
